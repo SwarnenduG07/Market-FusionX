84477a82c62ee57774324af06215095f72bf720b
Sunny <swarnendughosh05@gmail.com>
2024-08-13 17:13:35 +0530

    buying and selling order added

--- orderbook-server/src/types.ts
+++ orderbook-server/src/types.ts
@@ -1,12 +1,11 @@
-import z from "zod";
+import { z } from "zod";
 
-export const OderSchema = z.object({
-    baseAsset: z.string(),
-    quoteAsset: z.string(),
-    price: z.number(),
-    quantity: z.number(),
-    side: z.enum(["buy", "shell"]),
-    type: z.enum(["limit", "market"]),
-    kind: z.enum(["ioc"]).optional(),
-
-})
+export const OrderInputSchema = z.object({
+  baseAsset: z.string(),
+  quoteAsset: z.string(),
+  price: z.number(),
+  quantity: z.number(),
+  side: z.enum(['buy', 'sell']),
+  type: z.enum(['limit', 'market']),
+  kind: z.enum(['ioc']).optional(),
+});
