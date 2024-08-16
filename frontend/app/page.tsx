b9d18f3abc014fe8b3599a0c1df7d74f648ba35b
Sunny <swarnendughosh05@gmail.com>
2024-08-16 19:01:20 +0530

    frontend init

---
+++ frontend/app/page.tsx
@@ -0,0 +1,12 @@
+import Markets from '@/components/Markets'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+         <Markets />
+    </main>
+  )
+}
+
+export default Home
---
+++ frontend/components/Markets.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import { Tickers } from "@/types"
+import { useEffect, useState } from "react"
+
+const Markets = () => {
+
+  const [tickers, setTickers] = useState<Tickers[]>()
+
+  // useEffect(() => {
+  //     getTickers().then((m) => setTickers(m))
+  // },[])
+  return (
+    <div className="">
+        
+    </div>
+  )
+}
+
+export default Markets
