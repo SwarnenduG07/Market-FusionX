e8aa8cda8d9972701947cfa4088c1476b0caddf5
Sunny <swarnendughosh05@gmail.com>
2024-08-17 23:29:10 +0530

    http client addded with tradeview

--- frontend/app/components/TradeView.tsx
+++ frontend/app/components/TradeView.tsx
@@ -1,16 +1,52 @@
-import { useRef } from "react"
+import { useEffect, useRef } from "react"
+import { ChartManager } from "../utils/ChartManager";
+import { KLine } from "../utils/types";
+import { getKlines } from "../utils/httpClient";
 
 export function TradeView({market } : {market: string}) {
  
  const chartRef = useRef<HTMLDivElement>(null);
- const chatManagerRef = useRef<ChartManager>
+ const chartManagerRef = useRef<ChartManager>(null);
 
+ useEffect(() => {
+   const init = async () => {
+    let klineData: KLine[] = [];
+    try {
+      klineData = await getKlines(market, "1h", Math.floor((new Date().getTime() - 1000 * 60 * 60 * 34 * 7) / 1000), Math.floor(new Date().getTime() / 1000));
+    } catch (e) {
+      console.log("Trade View",e);
+    }
+    if (chartRef) {
+      if (chartManagerRef.current) {
+        chartManagerRef.current.destroy();
+      }
+      console.log(klineData)
+      const chartManager = new ChartManager(
+        chartRef.current,
+        [
+          ...klineData?.map((x) => ({
+            close: parseFloat(x.close),
+            high: parseFloat(x.high),
+            low: parseFloat(x.low),
+            open: parseFloat(x.open),
+            timestamp: new Date(x.end), 
+          })),
+        ].sort((x, y) => (x.timestamp < y.timestamp ? -1 : 1)) || [],
+        {
+          background: "#0e0f14",
+          color: "white",
+        }
+      );
+      //@ts-ignore
+      chartManagerRef.current = chartManager;
+    }
+  };
+  init();
+}, [market, chartRef]);
 
-     return (
-        <>
-           <div ref={chartRef}>
-
-           </div>
-        </>
-     )
+return (
+  <>
+    <div ref={chartRef} style={{ height: "520px", width: "100%", marginTop: 4 }}></div>
+  </>
+);
 }
--- frontend/app/trade/[market]/page.tsx
+++ frontend/app/trade/[market]/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { MarketBar } from '@/app/components/MarketBar'
+import { TradeView } from '@/app/components/TradeView'
 import { useParams } from 'next/navigation'
 import React from 'react'
 
@@ -11,7 +12,10 @@ export default function Page ()  {
           <MarketBar market={market as string} /> 
           <div className='flex flex-row h-[920px] border-y border-red-500'>
              <div className='flex flex-col flex-1'>
-
+                 <TradeView market={market as string} />
+             </div>
+             <div className='flex flex-col w-[250px] overflow-hidden'>
+               {/* <Depth /> */}
              </div>
           </div>
        </div>
